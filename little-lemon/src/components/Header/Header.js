68708ab202bcfbf7ea045009744fb184fb1c42ea
MohammedZohaQureshi-lab <61501597+MohammedZohaQureshi-lab@users.noreply.github.com>
2025-07-13 22:17:00 +0530

    Html & met tag updates

---
+++ little-lemon/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="/logo192.png" alt="Little Lemon Logo" className="header-logo" />
+      <h1 className="header-title">Little Lemon Restaurant</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ little-lemon/src/components/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Navigation from '../Navigation/Navigation'
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
+
+const HomePage = () => {
+  return (
+    <>
+      <Header/>
+      <Navigation/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default HomePage
